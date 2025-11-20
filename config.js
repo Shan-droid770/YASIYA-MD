88fed565dd1319f4972e3e640b3b86c802d3ce18
Shan-droid770 <kumaradissanayaka30@gmail.com>
2025-11-20 18:09:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?xoYQLM'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?Pm0eO5'
 };
